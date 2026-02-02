2753c8ba4aaa5bf8a8d4e012195c44b5e03211de
tim <timyates100@gmail.com>
2026-02-02 08:57:42 +0000

    dev vite stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,17 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default ({ mode }: { mode: string }) => {
   // load env into `process.env` as well as return an object
   const env = loadEnv(mode, process.cwd());
 
-  const backendProxy = env.VITE_BACKEND_FASTAPI_PROXY;
-
   const serverConfig: any = {};
 
-  if (backendProxy) {
+  if (mode === "development") {
     serverConfig.proxy = {
-      '/auth': {
-        target: backendProxy,
+      "/auth": {
+        target: env.VITE_BACKEND_FASTAPI_PROXY,
         changeOrigin: true,
       },
     };
